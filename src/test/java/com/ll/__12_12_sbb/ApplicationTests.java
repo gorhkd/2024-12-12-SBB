bd7e2d5b4f6c2461dc651b4626e6a0da36fdd93f
gorhkd <gorhkd223@gmail.com>
2024-12-12 22:14:55 +0900

    Test JPA Deleted

--- src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
+++ src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -18,13 +19,14 @@ class ApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
-    @DisplayName("수정")
-    void testJpaModify() {
+    @DisplayName("삭제")
+    void testJpaDeleted() {
+        assertEquals(2, questionRepository.count());
         Optional<Question> oq = this.questionRepository.findById(1);
         assertTrue(oq.isPresent());
-       Question q = oq.get();
-       q.setSubject("수정된 제목");
-       this.questionRepository.save(q);  // 알아서 기존 객체를 찾아서 갱신함.
+        Question q = oq.get();
+        this.questionRepository.delete(q);
+        assertEquals(1, this.questionRepository.count());
     }
 
 }

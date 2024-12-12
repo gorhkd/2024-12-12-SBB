8c8c5a112018c398b68d8bcd23a92556322565f9
gorhkd <gorhkd223@gmail.com>
2024-12-12 23:33:06 +0900

    Test JPA Question Dictionary From Answer

--- src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
+++ src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
@@ -1,7 +1,6 @@
 package com.ll.__12_12_sbb;
 
 import com.ll.__12_12_sbb.Entity.Answer;
-import com.ll.__12_12_sbb.Entity.Question;
 import com.ll.__12_12_sbb.Repository.AnswerRepository;
 import com.ll.__12_12_sbb.Repository.QuestionRepository;
 import org.junit.jupiter.api.DisplayName;
@@ -9,9 +8,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
@@ -24,18 +23,12 @@ class ApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
-    @DisplayName("답변저장")
-    void JpaAnswerSave() {
+    @DisplayName("답변조회")
+    void JpaAnswerDictionary() {
 
-        Optional<Question> oq = this.questionRepository.findById(2);
-        assertTrue(oq.isPresent());
-        Question q = oq.get();
-
-        Answer a = new Answer();
-        a.setContent("네 자동으로 생성됩니다.");
-        a.setQuestion(q);
-        a.setCreateDate(LocalDateTime.now());
-        answerRepository.save(a);
-    }
-
+        Optional<Answer> ao = this.answerRepository.findById(1);
+        assertTrue(ao.isPresent());
+         Answer a = ao.get();
+         assertEquals(2 , a.getQuestion().getId());
+        }
 }

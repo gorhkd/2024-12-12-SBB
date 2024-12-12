20a590cdeba83da30a6dd67eea476fc24bbfe56f
gorhkd <gorhkd223@gmail.com>
2024-12-12 16:37:28 +0900

    Create Repository

---
+++ src/main/java/com/ll/__12_12_sbb/Repository/AnswerRepository.java
@@ -0,0 +1,8 @@
+package com.ll.__12_12_sbb.Repository;
+
+import com.ll.__12_12_sbb.Entity.Answer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AnswerRepository extends JpaRepository<Answer, Integer> {
+
+}
---
+++ src/main/java/com/ll/__12_12_sbb/Repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package com.ll.__12_12_sbb.Repository;
+
+import com.ll.__12_12_sbb.Entity.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}

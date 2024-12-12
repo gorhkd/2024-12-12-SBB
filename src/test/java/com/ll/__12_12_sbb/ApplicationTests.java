2ebe77f3e4c8d98db91f0394c9c408b11f9be254
gorhkd <gorhkd223@gmail.com>
2024-12-12 23:23:34 +0900

    Test JPA Answer Save

--- src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
+++ src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
@@ -1,32 +1,41 @@
 package com.ll.__12_12_sbb;
 
+import com.ll.__12_12_sbb.Entity.Answer;
 import com.ll.__12_12_sbb.Entity.Question;
+import com.ll.__12_12_sbb.Repository.AnswerRepository;
 import com.ll.__12_12_sbb.Repository.QuestionRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.time.LocalDateTime;
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class ApplicationTests {
 
     @Autowired
+    private AnswerRepository answerRepository;
+
+    @Autowired
     private QuestionRepository questionRepository;
 
     @Test
-    @DisplayName("삭제")
-    void testJpaDeleted() {
-        assertEquals(2, questionRepository.count());
-        Optional<Question> oq = this.questionRepository.findById(1);
+    @DisplayName("답변저장")
+    void JpaAnswerSave() {
+
+        Optional<Question> oq = this.questionRepository.findById(2);
         assertTrue(oq.isPresent());
         Question q = oq.get();
-        this.questionRepository.delete(q);
-        assertEquals(1, this.questionRepository.count());
+
+        Answer a = new Answer();
+        a.setContent("네 자동으로 생성됩니다.");
+        a.setQuestion(q);
+        a.setCreateDate(LocalDateTime.now());
+        answerRepository.save(a);
     }
 
 }

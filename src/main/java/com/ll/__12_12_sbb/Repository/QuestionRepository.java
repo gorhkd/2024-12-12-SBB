c0ca51891a2b2c7c86efe92390b12c261fab2b13
gorhkd <gorhkd223@gmail.com>
2024-12-12 21:49:18 +0900

    TEST JPA Dictionary Fome Like

--- src/main/java/com/ll/__12_12_sbb/Repository/QuestionRepository.java
+++ src/main/java/com/ll/__12_12_sbb/Repository/QuestionRepository.java
@@ -3,9 +3,13 @@ package com.ll.__12_12_sbb.Repository;
 import com.ll.__12_12_sbb.Entity.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
+
+    List<Question> findBySubjectLike(String subject);
 }
--- src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
+++ src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
@@ -2,10 +2,12 @@ package com.ll.__12_12_sbb;
 
 import com.ll.__12_12_sbb.Entity.Question;
 import com.ll.__12_12_sbb.Repository.QuestionRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -17,6 +19,7 @@ class ApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
+    @DisplayName("Id 컬럼은 기본 메서드")
     void testJpaDictionary() {
         Optional<Question> oq = this.questionRepository.findById(1);
         if(oq.isPresent()){
@@ -26,14 +29,28 @@ class ApplicationTests {
     }
 
     @Test
+    @DisplayName("Id를 제외한 컬럼은 findBy + 컬럼명")
     void testJpaDictionaryFromSubject() {
         Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
         assertEquals(1, q.getId());
     }
 
     @Test
+    @DisplayName("findBy + 컬럼명 + And + 컬럼명")
     void testJpaDictionaryFromSubjectAndContent() {
         Question q = this.questionRepository.findBySubjectAndContent("sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
         assertEquals(1, q.getId());
     }
+
+    @Test
+    @DisplayName("findBy + 컬럼명 + Like")
+    void testJpaDictionaryFromLike() {
+        List<Question> qLisst = this.questionRepository.findBySubjectLike("sbb%");
+        Question q = qLisst.get(0);
+        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    }
+
+
+
+
 }

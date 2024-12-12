e511220a784287035571db9a58351f0fa086580c
gorhkd <gorhkd223@gmail.com>
2024-12-12 17:15:02 +0900

    Test JPA Dictionary From Id

--- src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
+++ src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -17,11 +17,11 @@ class ApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
-    void testJpaDictionary() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Question q = all.get(0);
-        assertEquals("sbb가 무엇인가요?", q.getSubject());
+    void testJpaDictionaryFromId() {
+        Optional<Question> oq = this.questionRepository.findById(1);
+        if(oq.isPresent()){
+            Question q = oq.get();
+            assertEquals("sbb가 무엇인가요?", q.getSubject());
+        }
     }
 }

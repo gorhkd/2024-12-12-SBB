688639403b1a1ffd3a3f388a6394878991664b0e
gorhkd <gorhkd223@gmail.com>
2024-12-12 17:20:51 +0900

    Test JPA Dictionary From Colunm

--- src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
+++ src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
@@ -17,11 +15,8 @@ class ApplicationTests {
     private QuestionRepository questionRepository;
 
     @Test
-    void testJpaDictionaryFromId() {
-        Optional<Question> oq = this.questionRepository.findById(1);
-        if(oq.isPresent()){
-            Question q = oq.get();
-            assertEquals("sbb가 무엇인가요?", q.getSubject());
-        }
+    void testJpaDictionaryFromSubject() {
+        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+        assertEquals(1, q.getId());
     }
 }

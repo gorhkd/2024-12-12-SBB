9b5e5efc0f495cefd507b1c05f5ed32f1f9f9dc2
gorhkd <gorhkd223@gmail.com>
2024-12-12 16:46:45 +0900

    Test JPA Save

--- src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
+++ src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
@@ -1,9 +1,31 @@
 package com.ll.__12_12_sbb;
 
+import com.ll.__12_12_sbb.Entity.Question;
+import com.ll.__12_12_sbb.Repository.QuestionRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class ApplicationTests {
 
+    @Autowired
+    private QuestionRepository questionRepository;
 
+    @Test
+    void testJpaSave() {
+        Question q1 = new Question();
+        q1.setSubject("sbb가 무엇인가요?");
+        q1.setContent("sbb에 대해서 알고 싶습니다.");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);
+
+        Question q2 = new Question();
+        q2.setSubject("스프링부트 모델 질문입니다.");
+        q2.setContent("id는 자동으로 생성되나요?");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);
+    }
 }

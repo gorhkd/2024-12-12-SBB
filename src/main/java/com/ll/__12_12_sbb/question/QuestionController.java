248fdc5ab391fbccdba0fa05f0fe3f9127604867
gorhkd <gorhkd223@gmail.com>
2024-12-23 09:59:30 +0900

    refactoring

--- src/main/java/com/ll/__12_12_sbb/question/QuestionController.java
+++ src/main/java/com/ll/__12_12_sbb/question/QuestionController.java
@@ -8,10 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @RequestMapping("/question")
 @RequiredArgsConstructor
@@ -21,7 +18,7 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String question(Model model, int page) {
+    public String question(Model model,@RequestParam(value="page", defaultValue="0") int page) {
         Page<Question> paging = this.questionService.getList(page);
         model.addAttribute("paging", paging);
         return "question_list";

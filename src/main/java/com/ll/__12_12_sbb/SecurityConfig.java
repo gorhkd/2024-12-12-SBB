341cb6ba46d0543e4bff227093217ffc24b1b7b2
gorhkd <gorhkd223@gmail.com>
2024-12-23 11:40:30 +0900

    signup create

--- src/main/java/com/ll/__12_12_sbb/SecurityConfig.java
+++ src/main/java/com/ll/__12_12_sbb/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -24,8 +26,13 @@ public class SecurityConfig {
                                 XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
         return http.build();
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
 // @Bean : SecurityFilterChain 빈을 생성하여 설정 가능하게 해주는 역할.
 // SecurityFilterChain : 모든 요청 URL에 필터를 적용하여 URL별로 특정 설정 가능.
 // 내부 메서드 : 인증되지 않은 모든 페이지의 요청을 허락한다. 라는 의미. (따라서 모든 페이지에 접근 가능)
-//
+
---
+++ src/main/java/com/ll/__12_12_sbb/user/UserService.java
@@ -0,0 +1,23 @@
+package com.ll.__12_12_sbb.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String name, String email, String password){
+        SiteUser user = new SiteUser();
+        user.setName(name);
+        user.setEmail(email);
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user; // 그냥 저장하면 끝 아니야? 왜 리턴?
+    }
+
+}

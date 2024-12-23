3d03b74346961bc0461e34b91fd4b64e75d50725
gorhkd <gorhkd223@gmail.com>
2024-12-23 10:27:52 +0900

    Security Create

---
+++ src/main/java/com/ll/__12_12_sbb/SecurityConfig.java
@@ -0,0 +1,31 @@
+package com.ll.__12_12_sbb;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration // 환경설정 역할
+@EnableWebSecurity // 영역 전개 역할
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                        .requestMatchers(new AntPathRequestMatcher("/**"))
+                .permitAll())
+                .csrf((csrf) -> csrf
+                        .ignoringRequestMatchers(new AntPathRequestMatcher("/h2-console/**")))
+                .headers((headers) -> headers
+                        .addHeaderWriter(new XFrameOptionsHeaderWriter(
+                                XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)));
+        return http.build();
+    }
+}
+// @Bean : SecurityFilterChain 빈을 생성하여 설정 가능하게 해주는 역할.
+// SecurityFilterChain : 모든 요청 URL에 필터를 적용하여 URL별로 특정 설정 가능.
+// 내부 메서드 : 인증되지 않은 모든 페이지의 요청을 허락한다. 라는 의미. (따라서 모든 페이지에 접근 가능)
+//

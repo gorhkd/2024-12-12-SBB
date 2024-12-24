4bff342f5417b9d06f5b9781db29d60a411952e8
gorhkd <gorhkd223@gmail.com>
2024-12-24 17:08:04 +0900

    2-6

--- src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
+++ src/test/java/com/ll/__12_12_sbb/ApplicationTests.java
@@ -1,21 +1,6 @@
 package com.ll.__12_12_sbb;
 
-import com.ll.__12_12_sbb.answer.Answer;
-import com.ll.__12_12_sbb.answer.AnswerRepository;
-import com.ll.__12_12_sbb.question.Question;
-import com.ll.__12_12_sbb.question.QuestionRepository;
-import jakarta.transaction.Transactional;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 // 1. 질문 데이터 저장
 // 2. 질문 데이터 조회 (Id, Subject, SubjectAndContent, SubjectLike)
@@ -29,107 +14,107 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @SpringBootTest
 class ApplicationTests {
 
-    @Autowired
-    private QuestionRepository questionRepository;
-    @Autowired
-    private AnswerRepository answerRepository;
-
-    @Test
-    @DisplayName("질문 데이터 저장")
-    void test1() {
-        Question q1 = new Question();
-        q1.setSubject("SBB 게시판 질문");
-        q1.setContent("질문을 어떻게하나요?");
-        q1.setCreateTime(LocalDateTime.now());
-        questionRepository.save(q1);
-
-        Question q2 = new Question();
-        q2.setSubject("SBB 게시판 답변");
-        q2.setContent("답변을 어떻게하나요?");
-        q2.setCreateTime(LocalDateTime.now());
-        questionRepository.save(q2);
-    }
-
-    @Test
-    @DisplayName("질문 데이터 조회")
-    void test2() {
-        List<Question> all = this.questionRepository.findAll();
-        assertEquals(2, all.size());
-
-        Optional<Question> q = this.questionRepository.findById(1);
-        if(q.isPresent()) {
-          assertEquals("SBB 게시판 질문", q.get().getSubject());
-        }
-
-        Question sub = this.questionRepository.findBySubject("SBB 게시판 질문");
-        assertEquals(1, sub.getId());
-
-        Question con = this.questionRepository.findByContent("답변을 어떻게하나요?");
-        assertEquals(2, con.getId());
-
-        Question sum = this.questionRepository.findBySubjectAndContent("SBB 게시판 질문", "질문을 어떻게하나요?");
-        assertEquals(1, sum.getId());
-
-        List<Question> questionList = this.questionRepository.findBySubjectLike("SBB%");
-        assertEquals(2, questionList.size());
-    }
-
-    @Test
-    @DisplayName("질문 수정")
-    void test3() {
-        Optional<Question> q = this.questionRepository.findById(3);
-        assertTrue(q.isPresent());
-        Question oq = q.get();
-        oq.setSubject("수정된 제목");
-        this.questionRepository.save(oq);
-    }
-
-    @Test
-    @DisplayName("질문 삭제")
-    void test4() {
-        assertEquals(4, this.questionRepository.count());
-        Optional<Question> q = this.questionRepository.findById(4);
-        assertTrue(q.isPresent());
-        Question qo = q.get();
-        this.questionRepository.delete(qo);
-        assertEquals(3, this.questionRepository.count());
-    }
-
-    @Test
-    @DisplayName("답변 데이터 저장하기")
-    void test5() {
-        Optional<Question> q = this.questionRepository.findById(1);
-        assertTrue(q.isPresent());
-        Question qo = q.get();
-
-        Answer answer = new Answer();
-        answer.setContent("질문을 하면 됩니다.");
-        answer.setCreateDate(LocalDateTime.now());
-        answer.setQuestion(qo);
-        this.answerRepository.save(answer);
-    }
-
-    @Test
-    @DisplayName("답변 데이터 조회")
-    void test6() {
-        Optional<Answer> a = this.answerRepository.findById(1);
-        assertTrue(a.isPresent());
-        Answer ao = a.get();
-        assertEquals(1, ao.getQuestion().getId());
-    }
-
-    @Transactional
-    @Test
-    @DisplayName("질문 데이터로 답변 찾기, 답변 데이터로 질문 찾기")
-    void test7() {
-        Optional<Question> q = this.questionRepository.findById(1);
-        assertTrue(q.isPresent());
-        Question qo = q.get();
-
-        List<Answer> answerList = qo.getAnswerList();
-
-        assertEquals(1, answerList.size());
-        assertEquals("질문을 하면 됩니다.", answerList.get(0).getContent());
-    }
+//    @Autowired
+//    private QuestionRepository questionRepository;
+//    @Autowired
+//    private AnswerRepository answerRepository;
+//
+//    @Test
+//    @DisplayName("질문 데이터 저장")
+//    void test1() {
+//        Question q1 = new Question();
+//        q1.setSubject("SBB 게시판 질문");
+//        q1.setContent("질문을 어떻게하나요?");
+//        q1.setCreateTime(LocalDateTime.now());
+//        questionRepository.save(q1);
+//
+//        Question q2 = new Question();
+//        q2.setSubject("SBB 게시판 답변");
+//        q2.setContent("답변을 어떻게하나요?");
+//        q2.setCreateTime(LocalDateTime.now());
+//        questionRepository.save(q2);
+//    }
+//
+//    @Test
+//    @DisplayName("질문 데이터 조회")
+//    void test2() {
+//        List<Question> all = this.questionRepository.findAll();
+//        assertEquals(2, all.size());
+//
+//        Optional<Question> q = this.questionRepository.findById(1);
+//        if(q.isPresent()) {
+//          assertEquals("SBB 게시판 질문", q.get().getSubject());
+//        }
+//
+//        Question sub = this.questionRepository.findBySubject("SBB 게시판 질문");
+//        assertEquals(1, sub.getId());
+//
+//        Question con = this.questionRepository.findByContent("답변을 어떻게하나요?");
+//        assertEquals(2, con.getId());
+//
+//        Question sum = this.questionRepository.findBySubjectAndContent("SBB 게시판 질문", "질문을 어떻게하나요?");
+//        assertEquals(1, sum.getId());
+//
+//        List<Question> questionList = this.questionRepository.findBySubjectLike("SBB%");
+//        assertEquals(2, questionList.size());
+//    }
+//
+//    @Test
+//    @DisplayName("질문 수정")
+//    void test3() {
+//        Optional<Question> q = this.questionRepository.findById(3);
+//        assertTrue(q.isPresent());
+//        Question oq = q.get();
+//        oq.setSubject("수정된 제목");
+//        this.questionRepository.save(oq);
+//    }
+//
+//    @Test
+//    @DisplayName("질문 삭제")
+//    void test4() {
+//        assertEquals(4, this.questionRepository.count());
+//        Optional<Question> q = this.questionRepository.findById(4);
+//        assertTrue(q.isPresent());
+//        Question qo = q.get();
+//        this.questionRepository.delete(qo);
+//        assertEquals(3, this.questionRepository.count());
+//    }
+//
+//    @Test
+//    @DisplayName("답변 데이터 저장하기")
+//    void test5() {
+//        Optional<Question> q = this.questionRepository.findById(1);
+//        assertTrue(q.isPresent());
+//        Question qo = q.get();
+//
+//        Answer answer = new Answer();
+//        answer.setContent("질문을 하면 됩니다.");
+//        answer.setCreateDate(LocalDateTime.now());
+//        answer.setQuestion(qo);
+//        this.answerRepository.save(answer);
+//    }
+//
+//    @Test
+//    @DisplayName("답변 데이터 조회")
+//    void test6() {
+//        Optional<Answer> a = this.answerRepository.findById(1);
+//        assertTrue(a.isPresent());
+//        Answer ao = a.get();
+//        assertEquals(1, ao.getQuestion().getId());
+//    }
+//
+//    @Transactional
+//    @Test
+//    @DisplayName("질문 데이터로 답변 찾기, 답변 데이터로 질문 찾기")
+//    void test7() {
+//        Optional<Question> q = this.questionRepository.findById(1);
+//        assertTrue(q.isPresent());
+//        Question qo = q.get();
+//
+//        List<Answer> answerList = qo.getAnswerList();
+//
+//        assertEquals(1, answerList.size());
+//        assertEquals("질문을 하면 됩니다.", answerList.get(0).getContent());
+//    }
 }
 
